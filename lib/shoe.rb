86a311b119f4bf528ee00645c5e9564154c85322
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-11-01 17:06:23 -0600

    commit tree 077ba96ef8b04f23d32dbeca8461ba431d21ae39

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+
+class Shoe 
+
+
+    def initialize brand="Adidas", color="red", size="9.5", material="suede", condition="tattered"
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    attr_accessor :brand, :color,:size, :material, :condition 
+
+    def cobble 
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
